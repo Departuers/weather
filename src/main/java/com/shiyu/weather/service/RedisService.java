012431b92402fdce74339ee51b63841c84d1c7bd
Departuers <2644631299@qq.com>
2019-10-04 16:32:53 +0800

    change

--- src/main/java/com/shiyu/weather/service/RedisService.java
+++ src/main/java/com/shiyu/weather/service/RedisService.java
@@ -1,6 +1,7 @@
 package com.shiyu.weather.service;
 
 public interface RedisService {
-     void set(String key, String value, long seconds);
-     Object get(String key);
+    void set(String key, String value, long seconds);
+
+    Object get(String key);
 }
